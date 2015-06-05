de1c69893266876efa84040a75431ac25f2de543
Christian Ihle <blurpy@gmail.com>
2015-06-05 09:01:25 +0200

    Legger til test som registrerer masse fanger med tilfeldige navn

--- src/test/java/no/mesan/reaktiv/helloworld/resources/MottakResourceIntegrationTest.java
+++ src/test/java/no/mesan/reaktiv/helloworld/resources/MottakResourceIntegrationTest.java
@@ -1,8 +1,18 @@
 package no.mesan.reaktiv.helloworld.resources;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import retrofit.RestAdapter;
@@ -13,6 +23,7 @@ import retrofit.client.Response;
  *
  * @author Christian Ihle
  */
+@Ignore
 public class MottakResourceIntegrationTest {
 
     private MottakRestService mottakRestService;
@@ -34,5 +45,39 @@ public class MottakResourceIntegrationTest {
 
         // 204: the server has fulfilled the request but does not need to return an entity-body
         assertEquals(204, response.getStatus());
+    }
+
+    @Test
+    public void startFangemottakMedMasseFolk() throws IOException, URISyntaxException {
+        final List<String> alleFornavn = hentAlleNavn("fornavn.txt");
+        final List<String> alleEtternavn = hentAlleNavn("etternavn.txt");
+
+        for (int i = 0; i < 100; i++) {
+            final String fornavn = velgTilfeldigNavn(alleFornavn);
+            final String etternavn = velgTilfeldigNavn(alleEtternavn);
+            final FangeDto fange = new FangeDto(fornavn, etternavn);
+
+            final Response response = mottakRestService.startFangemottak(fange);
+            assertEquals(204, response.getStatus());
+        }
+    }
+
+    private List<String> hentAlleNavn(final String fil) throws URISyntaxException, IOException {
+        final URL resource = getClass().getClassLoader().getResource(fil);
+        assertNotNull(resource);
+
+        final Path path = Paths.get(resource.toURI());
+
+        return Files.readAllLines(path);
+    }
+
+    private String velgTilfeldigNavn(final List<String> navn) {
+        return navn.get(velgTilfeldigTall(0, navn.size() -1));
+    }
+
+    private int velgTilfeldigTall(final int min, final int max) {
+        final int range = (max - min) + 1;
+
+        return (int)(Math.random() * range) + min;
     }
 }
